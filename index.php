4b48c9b4a2e67ad1bcd9afa398922b86abaf4e16
Johan Nordström <johan@digitalvillage.se>
2019-03-19 15:25:42 +0100

    Contain all the stuff!

--- index.php
+++ index.php
@@ -2,22 +2,27 @@
 get_header();
 ?>
 
-<h1>Hello, world!</h1>
+<div class="container">
+	<h1>Welcome to My Basic Theme</h1>
 
-<!-- Do we have any posts? -->
-<?php if (have_posts()) : ?>
-	<!-- Yey, we has posts -->
-	<?php while (have_posts()) : the_post(); ?>
-		<!-- This is a Blog Post -->
-		<h1><?php the_title(); ?></h1>
+	<h5>index.php</h5>
 
-		<?php the_content(); ?>
+	<!-- Do we have any posts? -->
+	<?php if (have_posts()) : ?>
+		<!-- Yey, we has posts -->
+		<?php while (have_posts()) : the_post(); ?>
+			<!-- This is a Blog Post -->
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<div>Post Created: <?php echo get_the_date(); ?> by <?php the_author(); ?> in <?php the_category(); ?></div>
 
-		<hr>
-		<!-- End of Blog Post -->
-	<?php endwhile; ?>
-<?php endif; ?>
-<!-- End of posts -->
+			<?php the_excerpt(); ?>
+
+			<hr>
+			<!-- End of Blog Post -->
+		<?php endwhile; ?>
+	<?php endif; ?>
+	<!-- End of posts -->
+</div><!-- /.container -->
 
 <?php
 get_footer();
