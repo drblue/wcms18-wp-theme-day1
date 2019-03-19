e90976748a851842d75c19374fdbc4a22c4d97e9
Johan Nordström <johan@digitalvillage.se>
2019-03-19 13:33:11 +0100

    A very basic loop.

--- index.php
+++ index.php
@@ -4,5 +4,20 @@ get_header();
 
 <h1>Hello, world!</h1>
 
+<!-- Do we have any posts? -->
+<?php if (have_posts()) : ?>
+	<!-- Yey, we has posts -->
+	<?php while (have_posts()) : the_post(); ?>
+		<!-- This is a Blog Post -->
+		<h1><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+
+		<hr>
+		<!-- End of Blog Post -->
+	<?php endwhile; ?>
+<?php endif; ?>
+<!-- End of posts -->
+
 <?php
 get_footer();
