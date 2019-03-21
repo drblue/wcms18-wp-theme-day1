72d5f6df476217fbaa96c76daa7669d8b8bd72ff
Johan Nordström <johan@digitalvillage.se>
2019-03-21 14:11:55 +0100

    Sidebar all the Widgets!

--- functions.php
+++ functions.php
@@ -2,9 +2,31 @@
 
 require("inc/bs4navwalker.php");
 
+/**
+ * Register menus for our theme.
+ *
+ * @return void
+ */
 function mbt_register_menus() {
 	register_nav_menus([
 		'main-menu' => 'Main Menu',
 	]);
 }
 add_action('init', 'mbt_register_menus');
+
+/**
+ * Register Theme Widget Areas.
+ *
+ * @return void
+ */
+function mbt_widgets_init() {
+	register_sidebar([
+		'name'			=> 'Blog Sidebar',
+		'id'			=> 'blog-sidebar',
+		'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'	=> '</aside>',
+		'before_title'	=> '<h1 class="widget-title">',
+		'after_title'	=> '</h1>',
+	]);
+}
+add_action('widgets_init', 'mbt_widgets_init');
--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,2 @@
-<h4>Sidebar</h4>
-
-<hr>
-
-<div>
-	<h5>Categories</h5>
-	<?php wp_list_categories(); ?>
-</div>
-
-<hr>
-
-<div>
-	<h5>Authors</h5>
-	<?php wp_list_authors(); ?>
-</div>
-
-<hr>
+<?php
+	dynamic_sidebar('blog-sidebar');
