890665b44e377133caa828be98bb101bb91e61ba
Johan Nordström <johan@digitalvillage.se>
2019-03-22 09:57:41 +0100

    Added JavaScript libraries to WordPress enqueue.

--- functions.php
+++ functions.php
@@ -37,6 +37,10 @@ add_action('widgets_init', 'mbt_widgets_init');
  * @return void
  */
 function mbt_register_scripts_and_styles() {
+	/**
+	 * Styles
+	 */
+
 	// Add Bootstrap CSS
 	wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', [], '4.3.1', 'all');
 
@@ -45,5 +49,21 @@ function mbt_register_scripts_and_styles() {
 
 	// Add Theme Print CSS
 	wp_enqueue_style('print-style', get_stylesheet_directory_uri() . '/assets/css/print.css', ['bootstrap', 'style'], null, 'print');
+
+	/**
+	 * Scripts
+	 */
+
+	// Remove WordPress jQuery
+	wp_deregister_script('jquery');
+
+	// Add jQuery
+	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', [], '3.3.1', true);
+
+	// Add popper.js
+	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', ['jquery'], '1.14.7', true);
+
+	// Add bootstrap.js
+	wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', ['jquery', 'popper'], '4.3.1', true);
 }
 add_action('wp_enqueue_scripts', 'mbt_register_scripts_and_styles');
