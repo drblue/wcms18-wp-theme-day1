1ece6e7e80841f15e39e7e202f50c2731263485b
Johan Nordström <johan@digitalvillage.se>
2019-03-27 13:16:45 +0100

    Ändrade så blogg-artikelns rubrik kommer före den utvalda bilden.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,11 +1,11 @@
 <!-- This is a Blog Post -->
+<h1><?php the_title(); ?></h1>
+
 <?php if (has_post_thumbnail()) : ?>
 	<div class="featured-image-wrapper">
 		<?php the_post_thumbnail('featured-image', ['class' => 'img-fluid d-block mx-auto']); ?>
 	</div>
 <?php endif; ?>
-
-<h1><?php the_title(); ?></h1>
 
 <?php the_content(); ?>
 
