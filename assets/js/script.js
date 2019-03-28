80f9b75e7a8a8c0a98e6fcc582c60299a2c301b2
Johan Nordström <johan@digitalvillage.se>
2019-03-28 10:47:54 +0100

    Lösningsförslag workshop dag 5, uppgift 4.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,10 @@
 	$(document).ready(function() {
 		// This will be run when DOM has signaled that it has finished rendering all elements
 		$('.card').on('click', function(e) {
-			console.log("Ooooh someone tickled me, here's the details:", e);
+			var $a = $(this).find('a');
+			if ($a.length > 0) {
+				window.location.href = $a.attr('href');
+			}
 		});
 	});
 
