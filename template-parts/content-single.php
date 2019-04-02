544804e020cbfc3853a01bc62a3db585660280b8
Johan Nordström <johan@digitalvillage.se>
2019-04-02 09:28:08 +0200

    Wrapped content in <article> element.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,16 +1,15 @@
 <!-- This is a Blog Post -->
-<h1><?php the_title(); ?></h1>
+<article>
+	<h1><?php the_title(); ?></h1>
 
-<?php if (has_post_thumbnail()) : ?>
-	<div class="featured-image-wrapper">
-		<?php the_post_thumbnail('featured-image', ['class' => 'img-fluid d-block mx-auto']); ?>
-	</div>
-<?php endif; ?>
+	<?php if (has_post_thumbnail()) : ?>
+		<div class="featured-image-wrapper">
+			<?php the_post_thumbnail('featured-image', ['class' => 'img-fluid d-block mx-auto']); ?>
+		</div>
+	<?php endif; ?>
 
-<?php the_content(); ?>
+	<p class="text-muted"><em>Post Created: <?php echo get_the_date(); ?> by <?php the_author(); ?></em></p>
 
-<hr>
-
-<div>Post Created: <?php echo get_the_date(); ?> by <?php the_author(); ?></div>
-
+	<?php the_content(); ?>
+</article>
 <!-- End of Blog Post -->
